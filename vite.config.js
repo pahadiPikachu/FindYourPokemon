f556e6ae6e5d37f5ae046a9ead3755b9fde6d0db
pahadiPikachu <s.1001akshi@gmail.com>
2024-10-13 02:10:30 +0200

    Deploying updated files to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/FindYourPokemon/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/FindYourPokemon/' : '/',
+  base: "/FindYourPokemon/",
   plugins: [react()],
 })
